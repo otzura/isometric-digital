f529fbabec83148354a6522764a3cf7d1c96f9e0
otzura <gaetan.slt@gmail.com>
2024-03-20 09:28:18 +0100

    first com

---
+++ fr-FR/contact_files/main.js
@@ -0,0 +1,71 @@
+$(function() {
+
+	'use strict';
+
+	// Form
+
+	var contactForm = function() {
+
+		if ($('#contactForm').length > 0 ) {
+			$( "#contactForm" ).validate( {
+				rules: {
+					fname: "required",
+					lname: "required",
+					email: {
+						required: true,
+						email: true
+					},
+					message: {
+						required: true,
+						minlength: 5
+					}
+				},
+				messages: {
+					fname: "Veuillez saisir votre prénom",
+					lname: "Veuillez saisir votre nom",
+					email: "Veuillez saisir une adresse électronique valide",
+					message: "Veuillez saisir un message"
+				},
+				/* submit via ajax */
+				submitHandler: function(form) {		
+					var $submit = $('.submitting'),
+						waitText = 'Envoi...';
+
+					$.ajax({   	
+				      type: "POST",
+				      url: "php/send-email.php",
+				      data: $(form).serialize(),
+
+				      beforeSend: function() { 
+				      	$submit.css('display', 'block').text(waitText);
+				      },
+				      success: function(msg) {
+		               if (msg == 'OK') {
+		               	$('#form-message-warning').hide();
+				            setTimeout(function(){
+		               		$('#contactForm').fadeOut();
+		               	}, 1000);
+				            setTimeout(function(){
+				               $('#form-message-success').fadeIn();   
+		               	}, 1400);
+			               
+			            } else {
+			               $('#form-message-warning').html(msg);
+				            $('#form-message-warning').fadeIn();
+				            $submit.css('display', 'none');
+			            }
+				      },
+				      error: function() {
+				      	$('#form-message-warning').html("Un problème s'est produit. Veuillez réessayer.");
+				         $('#form-message-warning').fadeIn();
+				         $submit.css('display', 'none');
+				      }
+			      });    		
+		  		}
+				
+			} );
+		}
+	};
+	contactForm();
+
+});
